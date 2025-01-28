969dc5fd9ee09b9a0cc97a48fc3c1907944ae00f
kristenak2005 <kristena.spruksta2@mail.dcu.ie>
2025-01-28 12:26:28 +0000

    make_configfile.py

--- make_configfile.py
+++ make_configfile.py
@@ -4,7 +4,7 @@ config = configparser.ConfigParser()
 
 config.add_section('directories')
 config.set('directories', 'data_dir', '/mnt/scratch/data/spruksk2/data/')
-config.set('directories', 'main_dir', '/mnt/scratch/data/spruksk2/python_output')
+config.set('directories', 'main_dir', '/mnt/scratch/data/spruksk2/python_output/FIP')
 
 with open(r"configfile.ini", 'w') as configfile:
     config.write(configfile)
