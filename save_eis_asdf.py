8de669751064df2d12d4e12910f7166b5cabc81d
kristenak2005 <kristena.spruksta2@mail.dcu.ie>
2025-02-10 12:12:36 +0000

    save_eis_asdf.py

--- save_eis_asdf.py
+++ save_eis_asdf.py
@@ -1,15 +1,8 @@
 import sunpy.map
 import matplotlib.pyplot as plt
 import astropy.units as u
+import asdf
 
-# Load your EIS FITS file
-eis_map = sunpy.map.Map("eis_20151018_102719_sis.fits")
+eis_map =sunpy.map.Map("eis_20151018_102719_sis.fits")
 
-# Plot the image with clipping
-#eis_map.peek()
-eis_map.peek(clip_interval=(1, 99.99) * u.percent)
-
-# Create a dictionary (ASDF tree) and save the file
-tree = {'eis_map': eis_map}
-with asdf.AsdfFile(tree) as asdf_file:
-    asdf_file.write_to("eis_map.asdf")
+eis_map.peek()
