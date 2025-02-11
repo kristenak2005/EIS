cb792b6b185254c26d3eddcb1463efc9160356aa
kristenak2005 <kristena.spruksta2@mail.dcu.ie>
2025-02-11 12:50:39 +0000

    save_eis_asdf.py

--- save_eis_asdf.py
+++ save_eis_asdf.py
@@ -3,6 +3,11 @@ import matplotlib.pyplot as plt
 import astropy.units as u
 import asdf
 
-eis_map =sunpy.map.Map("eis_20151018_102719_sis.fits")
+eis_map =sunpy.map.Map("eis_20151018_102719_composition_sis.asdf")
 
 eis_map.peek()
+
+
+with asdf.open('eis)20151018_102719_composition_sis.asdf') as f:
+  composition= f'['composition'] 
+  print(composition)
