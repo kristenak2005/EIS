3a43ba7c5f6fae2e0ddeda09f38c87c41e5c18b8
kristenak2005 <kristena.spruksta2@mail.dcu.ie>
2025-02-24 16:37:54 +0000

    processes_2.py

--- processes_2.py
+++ processes_2.py
@@ -154,13 +154,14 @@ def get_context_img(img_time):
 # Define a function based on Andy's asheis.py class to fit the data for a defined line to get a defined property
 
 # %%
+###changed ncpu to 20 hopefully works
 def fit_data(file,fitted_lines,line,product,output_location):
     template_name = fitted_lines[f'{line}'][0]
     path = output_location+'/'+file.split('/')[5][0:19]+'.'+(f'{template_name}'.replace(".template",f"-{fitted_lines[f'{line}'][1]}.fit"))
     if os.path.isfile(path) == False:
         template = eispac.read_template(eispac.data.get_fit_template_filepath(template_name))
         cube = eispac.read_cube(file, window=template.central_wave)
-        fit_res = eispac.fit_spectra(cube, template, ncpu='10')
+        fit_res = eispac.fit_spectra(cube, template, ncpu='20')
         save_filepaths = eispac.save_fit(fit_res, save_dir=output_location)
         print(save_filepaths)
     else:
@@ -234,18 +235,42 @@ def get_composition(linepair, filename, output_location, fitted_lines):
 #    m_upr = Map(outfiles[0])
 #    m_lwr = Map(outfiles[1])
 
+    #f_upr = glob.glob(output_location+'/EIS_fit_'+lines[0]+'.asdf')
+    #arrs_upr = asdf.open(f_upr[0])
+    #m_upr = arrs_upr['int_map']
+
     f_upr = glob.glob(output_location+'/EIS_fit_'+lines[0]+'.asdf')
+    if not f_upr:
+        raise FileNotFoundError(f"Upper line file not found: {output_location+'/EIS_fit_'+lines[0]+'.asdf'}")
     arrs_upr = asdf.open(f_upr[0])
     m_upr = arrs_upr['int_map']
 
     f_lwr = glob.glob(output_location+'/EIS_fit_'+lines[1]+'.asdf')
+    if not f_lwr:
+        raise FileNotFoundError(f"Lower line file not found: {output_location+'/EIS_fit_'+lines[1]+'.asdf'}")
     arrs_lwr = asdf.open(f_lwr[0])
     m_lwr = arrs_lwr['int_map']
+    
+    #f_lwr = glob.glob(output_location+'/EIS_fit_'+lines[1]+'.asdf')
+    #arrs_lwr = asdf.open(f_lwr[0])
+    #m_lwr = arrs_lwr['int_map']
 
-    m_comp = m_upr
-    m_comp.meta['line_id'] = lines[2]
+    #m_comp = m_upr
+    #m_comp.meta['line_id'] = lines[2]
+    #m_comp = Map(m_upr.data/m_lwr.data, m_upr.meta)
+    #m_comp.save(output_location+'/composition_files/eis_'+m_comp.date.strftime('%Y%m%d_%H%M%S')+'_composition_'+linepair+'.fits', overwrite=True)
+    #return m_comp
+
     m_comp = Map(m_upr.data/m_lwr.data, m_upr.meta)
-    m_comp.save(output_location+'/composition_files/eis_'+m_comp.date.strftime('%Y%m%d_%H%M%S')+'_composition_'+linepair+'.fits', overwrite=True)
+    m_comp.meta['line_id'] = lines[2]
+
+    # ✅ Ensure the output directory exists before saving
+    save_dir = os.path.join(output_location, 'composition_files')
+    os.makedirs(save_dir, exist_ok=True)  # Creates the directory if it doesn't exist
+
+    output_filename = os.path.join(save_dir, f'eis_{m_comp.date.strftime("%Y%m%d_%H%M%S")}_composition_{linepair}.fits')
+
+    m_comp.save(output_filename, overwrite=True)
     return m_comp
 
 # %% [markdown]
