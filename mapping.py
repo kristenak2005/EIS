9e677585e5999055c1d44978e2825b5118f031ef
kristenak2005 <kristena.spruksta2@mail.dcu.ie>
2025-03-06 11:28:00 +0000

    mapping.py

--- mapping.py
+++ mapping.py
@@ -3,18 +3,8 @@ import asdf
 
 with asdf.open("EIS_fit_fe_12_195.asdf") as af:
     intensity_map = af["int_map"].data
-    plt.imshow(intensity_map. cmap="inferno", origin="lower")
+    plt.imshow(intensity_map. cmap="RdYlBu", origin="lower")
     plt.title("EIS fit FE XII 195")
     plt.show()
 
 
-
-
-# Plot with better scaling
-plt.figure(figsize=(8, 6))  # Adjust figure size
-plt.imshow(intensity_map, cmap="inferno", origin="lower", aspect="equal")  # ✅ Maintain aspect ratio
-plt.colorbar(label="Intensity")  # Add a colorbar
-plt.title("EIS Fit Fe XII 195")
-plt.xlabel("X Pixel")
-plt.ylabel("Y Pixel")
-plt.show()
