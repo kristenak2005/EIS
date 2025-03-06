4e507fd56a7d63d13816609fba22c67f07f052a3
kristenak2005 <kristena.spruksta2@mail.dcu.ie>
2025-03-06 11:44:51 +0000

    mapping.py

--- mapping.py
+++ mapping.py
@@ -3,7 +3,7 @@ import asdf
 
 with asdf.open("EIS_fit_fe_12_195.asdf") as af:
     intensity_map = af["int_map"].data
-    plt.imshow(intensity_map. cmap="RdYlBu", origin="lower")
+    plt.imshow(intensity_map, cmap="RdYlBu", origin="lower")
     plt.title("EIS fit FE XII 195")
     plt.show()
 
