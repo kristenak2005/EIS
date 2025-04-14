2728b632e51766680fbe99b1b0013054b7044202
kristenak2005 <kristena.spruksta2@mail.dcu.ie>
2025-04-14 11:17:21 +0100

    velocity_composite_map.py

--- velocity_composite_map.py
+++ velocity_composite_map.py
@@ -35,74 +35,101 @@ i_map20 = Map("/mnt/scratch/data/spruksk2/python_output/EIS_work/20151019_163612
 bottom_left = [200, -800] * u.arcsec
 top_right = [1000, -200] * u.arcsec
 
-i_smap = i_map.submap(SkyCoord(*bottom_left, frame=i_map.coordinate_frame),
-                      top_right=SkyCoord(*top_right, frame=i_map.coordinate_frame))
+#i_smap = i_map.submap(SkyCoord(*bottom_left, frame=i_map.coordinate_frame),
+ #                     top_right=SkyCoord(*top_right, frame=i_map.coordinate_frame))
 
-i_smap1 = i_map1.submap(SkyCoord(*bottom_left, frame=i_map1.coordinate_frame),
-                      top_right=SkyCoord(*top_right, frame=i_map1.coordinate_frame))
+#i_smap1 = i_map1.submap(SkyCoord(*bottom_left, frame=i_map1.coordinate_frame),
+ #                     top_right=SkyCoord(*top_right, frame=i_map1.coordinate_frame))
 
-i_smap2 = i_map2.submap(SkyCoord(*bottom_left, frame=i_map2.coordinate_frame),
-                      top_right=SkyCoord(*top_right, frame=i_map2.coordinate_frame))
+#i_smap2 = i_map2.submap(SkyCoord(*bottom_left, frame=i_map2.coordinate_frame),
+ #                     top_right=SkyCoord(*top_right, frame=i_map2.coordinate_frame))
 
-i_smap3 = i_map3.submap(SkyCoord(*bottom_left, frame=i_map3.coordinate_frame),
-                      top_right=SkyCoord(*top_right, frame=i_map3.coordinate_frame))
+#i_smap3 = i_map3.submap(SkyCoord(*bottom_left, frame=i_map3.coordinate_frame),
+ #                     top_right=SkyCoord(*top_right, frame=i_map3.coordinate_frame))
 
-i_smap4 = i_map4.submap(SkyCoord(*bottom_left, frame=i_map4.coordinate_frame),
-                      top_right=SkyCoord(*top_right, frame=i_map4.coordinate_frame))
+#i_smap4 = i_map4.submap(SkyCoord(*bottom_left, frame=i_map4.coordinate_frame),
+ #                     top_right=SkyCoord(*top_right, frame=i_map4.coordinate_frame))
 
-i_smap5 = i_map5.submap(SkyCoord(*bottom_left, frame=i_map5.coordinate_frame),
-                      top_right=SkyCoord(*top_right, frame=i_map5.coordinate_frame))
+#i_smap5 = i_map5.submap(SkyCoord(*bottom_left, frame=i_map5.coordinate_frame),
+ #                     top_right=SkyCoord(*top_right, frame=i_map5.coordinate_frame))
 
-i_smap6 = i_map6.submap(SkyCoord(*bottom_left, frame=i_map6.coordinate_frame),
-                      top_right=SkyCoord(*top_right, frame=i_map6.coordinate_frame))
+#i_smap6 = i_map6.submap(SkyCoord(*bottom_left, frame=i_map6.coordinate_frame),
+ #                     top_right=SkyCoord(*top_right, frame=i_map6.coordinate_frame))
 
-i_smap7 = i_map7.submap(SkyCoord(*bottom_left, frame=i_ma7p.coordinate_frame),
-                      top_right=SkyCoord(*top_right, frame=i_map7.coordinate_frame))
+#i_smap7 = i_map7.submap(SkyCoord(*bottom_left, frame=i_map7.coordinate_frame),
+ #                     top_right=SkyCoord(*top_right, frame=i_map7.coordinate_frame))
 
-i_smap8 = i_map8.submap(SkyCoord(*bottom_left, frame=i_map8.coordinate_frame),
-                      top_right=SkyCoord(*top_right, frame=i_map8.coordinate_frame))
+#i_smap8 = i_map8.submap(SkyCoord(*bottom_left, frame=i_map8.coordinate_frame),
+ #                     top_right=SkyCoord(*top_right, frame=i_map8.coordinate_frame))
 
-i_smap9 = i_map9.submap(SkyCoord(*bottom_left, frame=i_map9.coordinate_frame),
-                      top_right=SkyCoord(*top_right, frame=i_map9.coordinate_frame))
+#i_smap9 = i_map9.submap(SkyCoord(*bottom_left, frame=i_map9.coordinate_frame),
+ #                     top_right=SkyCoord(*top_right, frame=i_map9.coordinate_frame))
 
-i_sma10 = i_map10.submap(SkyCoord(*bottom_left, frame=i_map10.coordinate_frame),
-                      top_right=SkyCoord(*top_right, frame=i_map10.coordinate_frame))
+#i_sma10 = i_map10.submap(SkyCoord(*bottom_left, frame=i_map10.coordinate_frame),
+ #                     top_right=SkyCoord(*top_right, frame=i_map10.coordinate_frame))
 
-i_smap11 = i_map11.submap(SkyCoord(*bottom_left, frame=i_map11.coordinate_frame),
-                      top_right=SkyCoord(*top_right, frame=i_map11.coordinate_frame))
+#i_smap11 = i_map11.submap(SkyCoord(*bottom_left, frame=i_map11.coordinate_frame),
+ #                     top_right=SkyCoord(*top_right, frame=i_map11.coordinate_frame))
 
-i_smap12 = i_map12.submap(SkyCoord(*bottom_left, frame=i_map12.coordinate_frame),
-                      top_right=SkyCoord(*top_right, frame=i_map12.coordinate_frame))
+#i_smap12 = i_map12.submap(SkyCoord(*bottom_left, frame=i_map12.coordinate_frame),
+ #                     top_right=SkyCoord(*top_right, frame=i_map12.coordinate_frame))
 
-i_smap13 = i_map13.submap(SkyCoord(*bottom_left, frame=i_map13.coordinate_frame),
-                      top_right=SkyCoord(*top_right, frame=i_map13.coordinate_frame))
+#i_smap13 = i_map13.submap(SkyCoord(*bottom_left, frame=i_map13.coordinate_frame),
+ #                     top_right=SkyCoord(*top_right, frame=i_map13.coordinate_frame))
 
-i_smap14 = i_map14.submap(SkyCoord(*bottom_left, frame=i_map14.coordinate_frame),
-                      top_right=SkyCoord(*top_right, frame=i_map14.coordinate_frame))
+#i_smap14 = i_map14.submap(SkyCoord(*bottom_left, frame=i_map14.coordinate_frame),
+ #                     top_right=SkyCoord(*top_right, frame=i_map14.coordinate_frame))
 
-i_smap15 = i_map15.submap(SkyCoord(*bottom_left, frame=i_map15.coordinate_frame),
-                      top_right=SkyCoord(*top_right, frame=i_map15.coordinate_frame))
+#i_smap15 = i_map15.submap(SkyCoord(*bottom_left, frame=i_map15.coordinate_frame),
+ #                     top_right=SkyCoord(*top_right, frame=i_map15.coordinate_frame))
 
-i_smap16 = i_map16.submap(SkyCoord(*bottom_left, frame=i_map16.coordinate_frame),
-                      top_right=SkyCoord(*top_right, frame=i_map16.coordinate_frame))
+#i_smap16 = i_map16.submap(SkyCoord(*bottom_left, frame=i_map16.coordinate_frame),
+ #                     top_right=SkyCoord(*top_right, frame=i_map16.coordinate_frame))
 
-i_smap17 = i_map17.submap(SkyCoord(*bottom_left, frame=i_map17.coordinate_frame),
-                      top_right=SkyCoord(*top_right, frame=i_map17.coordinate_frame))
+#i_smap17 = i_map17.submap(SkyCoord(*bottom_left, frame=i_map17.coordinate_frame),
+ #                     top_right=SkyCoord(*top_right, frame=i_map17.coordinate_frame))
+
+#i_smap18 = i_map18.submap(SkyCoord(*bottom_left, frame=i_map18.coordinate_frame),
+ #                     top_right=SkyCoord(*top_right, frame=i_map18.coordinate_frame))
+
+#i_smap19 = i_map19.submap(SkyCoord(*bottom_left, frame=i_map19.coordinate_frame),
+ #                     top_right=SkyCoord(*top_right, frame=i_map19.coordinate_frame))
+
+#i_smap20 = i_map20.submap(SkyCoord(*bottom_left, frame=i_map20.coordinate_frame),
+ #                     top_right=SkyCoord(*top_right, frame=i_map20.coordinate_frame))
 
 
 
+
+min_value = 0
+max_value = 4
 
 fig = plt.figure()
 ax = fig.add_subplot(projection=i_map)
-i_map.plot(axes=ax, clip_interval=(1, 99.9) * u.percent, zorder=0, autoalign=True, cmap='RdYlBu'))
-i_map1.plot(axes=ax, clip_interval=(1, 99.97) * u.percent, alpha=0.7, zorder=1, autoalign=True)
-i_map2.plot(axes=ax, clip_interval=(1, 99.97) * u.percent, alpha=0.7, zorder=2, autoalign=True)
-i_map3.plot(axes=ax, clip_interval=(1, 99.97) * u.percent, alpha=0.7, zorder=3, autoalign=True)
-i_map4.plot(axes=ax, clip_interval=(1, 99.97) * u.percent, alpha=0.7, zorder=4, autoalign=True)
-i_map5.plot(axes=ax, clip_interval=(1, 99.97) * u.percent, alpha=0.7, zorder=5, autoalign=True)
-i_map6.plot(axes=ax, clip_interval=(1, 99.97) * u.percent, alpha=0.7, zorder=6, autoalign=True)
-i_map7.plot(axes=ax, clip_interval=(1, 99.97) * u.percent, alpha=0.7, zorder=7, autoalign=True)
-i_map8.plot(axes=ax, clip_interval=(1, 99.97) * u.percent, alpha=0.7, zorder=8, autoalign=True)
-i_map9.plot(axes=ax, clip_interval=(1, 99.97) * u.percent, alpha=0.7, zorder=9, autoalign=True)
+i_map.plot(axes=ax, clip_interval=(min_value, max_value) * u.percent, zorder=0,autoalign=True, cmap='OrRd')
+i_map1.plot(axes=ax, clip_interval=(min_value, max_value) * u.percent, alpha=0.7, zorder=1, autoalign=True, cmap='OrRd')
+i_map2.plot(axes=ax, clip_interval=(min_value, max_value) * u.percent, alpha=0.7, zorder=2, autoalign=True, cmap='OrRd')
+i_map3.plot(axes=ax, clip_interval=(min_value, max_value) * u.percent, alpha=0.7, zorder=3, autoalign=True, cmap='OrRd')
+i_map4.plot(axes=ax, clip_interval=(min_value, max_value) * u.percent, alpha=0.7, zorder=4, autoalign=True, cmap='OrRd')
+i_map5.plot(axes=ax, clip_interval=(min_value, max_value) * u.percent, alpha=0.7, zorder=5, autoalign=True, cmap='OrRd')
+i_map6.plot(axes=ax, clip_interval=(min_value, max_value) * u.percent, alpha=0.7, zorder=6, autoalign=True, cmap='OrRd')
+i_map7.plot(axes=ax, clip_interval=(min_value, max_value) * u.percent, alpha=0.7, zorder=7, autoalign=True, cmap='OrRd')
+i_map8.plot(axes=ax, clip_interval=(min_value, max_value) * u.percent, alpha=0.7, zorder=8, autoalign=True, cmap='OrRd')
+i_map9.plot(axes=ax, clip_interval=(min_value, max_value) * u.percent, alpha=0.7, zorder=9, autoalign=True, cmap='OrRd')
+i_map10.plot(axes=ax, clip_interval=(min_value, max_value) * u.percent, alpha=0.7, zorder=10, autoalign=True, cmap='OrRd')
+i_map11.plot(axes=ax, clip_interval=(min_value, max_value) * u.percent, alpha=0.7, zorder=11, autoalign=True, cmap='OrRd')
+i_map12.plot(axes=ax, clip_interval=(min_value, max_value) * u.percent, alpha=0.7, zorder=12, autoalign=True, cmap='OrRd')
+i_map13.plot(axes=ax, clip_interval=(min_value, max_value) * u.percent, alpha=0.7, zorder=13, autoalign=True, cmap='OrRd')
+i_map14.plot(axes=ax, clip_interval=(min_value, max_value) * u.percent, alpha=0.7, zorder=14, autoalign=True, cmap='OrRd')
+i_map15.plot(axes=ax, clip_interval=(min_value, max_value) * u.percent, alpha=0.7, zorder=15, autoalign=True, cmap='OrRd')
+i_map16.plot(axes=ax, clip_interval=(min_value, max_value) * u.percent, alpha=0.7, zorder=16, autoalign=True, cmap='OrRd')
+i_map17.plot(axes=ax, clip_interval=(min_value, max_value) * u.percent, alpha=0.7, zorder=17, autoalign=True, cmap='OrRd')
+i_map18.plot(axes=ax, clip_interval=(min_value, max_value) * u.percent, alpha=0.7, zorder=18, autoalign=True, cmap='OrRd')
+i_map19.plot(axes=ax, clip_interval=(min_value, max_value) * u.percent, alpha=0.7, zorder=19, autoalign=True, cmap='OrRd')
+i_map20.plot(axes=ax, clip_interval=(min_value, max_value) * u.percent, alpha=0.7, zorder=20, autoalign=True, cmap='OrRd')
+
+aspect_ratio = i_map.meta['cdelt2'] / i_map.meta['cdelt1']
+ax.set_aspect(aspect_ratio)
+
 ax.set_title("EIS Velocity Composite Map")
 plt.show()
